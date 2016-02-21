09dbfb0b30534800df8bfa7e8a9e6e798d90d8f7
Shafiq Daniel <shafdanny@gmail.com>
2016-02-21 16:44:16 +0100

    Diffusion horizontale

--- src/plaque.c
+++ src/plaque.c
@@ -2,18 +2,32 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
+
 #define TEMP_CHAUD 255
 #define TEMP_FROID 0
 
-float cell[16][16];
+#define H 6
+
+bool isZoneInterne(int i, int s);
+
+
 float *cellPrev = NULL;
 float *cellCurr = NULL;
+
+
+
+void copyPlaque(float *plOrigin, float *plDest, int nbCell) {
+	int i;
+	for(i=0; i<nbCell; i++){
+		plDest[i] = plOrigin[i];
+	}
+}
 
 /**
  * Afficher les cellules. Utilisation pour debogage, 
  * seulement pour des petits tailles.
  */
-void display(float *cell, int nbLigne){
+void display(float *cell, int nbLigne) {
 	int i,j;
 	
 	for(i=0; i<nbLigne*nbLigne; i++){
@@ -22,6 +36,45 @@ void display(float *cell, int nbLigne){
 		printf("|%4.0f| ", cell[i]);
 	}
 	printf("\n");
+}
+
+void diffusionHorizontale() {
+	int i;
+
+	for(i=0; i<16*16; i++) {
+		//printf("Checking cell %d \n", i);
+		if(!isZoneInterne(i,0)) {
+			cellCurr[i] = (cellPrev[i+1] + (H-2)*(cellPrev[i]) + cellPrev[i-1])/H;
+		}
+
+		/**
+		if(!isZoneInterne(i+1,0)) {
+			//printf("Updating right cell %d with value %f \n", i+1, cellPrev[i]/6);
+			cellCurr[i+1] += cellPrev[i+1] + cellPrev[i]/6;
+		}
+
+		if(!isZoneInterne(i-1,0)) {
+			//printf("Updating left  cell %d with value %f \n", i-1, cellPrev[i]/6);
+			cellCurr[i-1] += cellPrev[i-1] + cellPrev[i]/6;
+		}**/
+	}
+}
+
+void updatePlaque() {
+	copyPlaque(cellCurr, cellPrev, 16*16);
+	
+	printf("before diffusion \n");
+
+	display(cellPrev, 16);
+	display(cellCurr, 16);
+
+	diffusionHorizontale();
+	diffusionVerticale();
+
+	printf("after diffusion \n");
+
+	display(cellPrev, 16);
+	display(cellCurr, 16);
 }
 
 /**
@@ -53,19 +106,24 @@ void plaqueInit(int s) {
 	
 	printf("Will create %d row and line, so %d cell \n", nbLigne, nbCell);
 	cellCurr = (float*)malloc(nbCell*sizeof(float));
-	
+	cellPrev = (float*)malloc(nbCell*sizeof(float));
+
+
 	int i=0;
 	int j=0;
 	
 	for(i=0; i<nbCell; i++){
-	
 		if(isZoneInterne(i,s))
 			cellCurr[i] = TEMP_CHAUD;
 		else
 			cellCurr[i] = TEMP_FROID;
 	}
 	
+	printf("cellCurr \n");
 	display(cellCurr, nbLigne);
+	printf("cellPrev \n");
+	display(cellPrev, nbLigne);
+	updatePlaque();
 }
 
 void printIntro(){
