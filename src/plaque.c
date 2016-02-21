13527bb32497854c4378da07ec6b04f605be2f1f
Shafiq Daniel <shafdanny@gmail.com>
2016-02-21 14:34:02 +0100

    Refactoring

--- src/plaque.c
+++ src/plaque.c
@@ -1,25 +1,45 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #define TEMP_CHAUD 255
 #define TEMP_FROID 0
 
 float cell[16][16];
-float *cellule = NULL;
+float *cellPrev = NULL;
+float *cellCurr = NULL;
 
 /**
  * Afficher les cellules. Utilisation pour debogage, 
  * seulement pour des petits tailles.
  */
-void display(int nbLigne){
+void display(float *cell, int nbLigne){
 	int i,j;
 	
 	for(i=0; i<nbLigne*nbLigne; i++){
 		if(i%nbLigne == 0)
 			printf("\n");
-		printf("|%4.0f| ", cellule[i]);
+		printf("|%4.0f| ", cell[i]);
 	}
 	printf("\n");
+}
+
+/**
+ * Verifier si une cellule est dans la zone interne.
+ */
+bool isZoneInterne(int i, int s) {
+	int nbLigne = 1 << ((s+4));
+
+	// Calcul de la zone interne, dans laquelle la temperature est constante
+	int highBound = 1 << (s+3);
+	int lowBound = 1 << s;
+	
+	int zoneInterneMin = highBound - lowBound;
+	int zoneInterneMax = highBound + lowBound;
+
+	return (i%nbLigne >= zoneInterneMin && i%nbLigne < zoneInterneMax
+			&& i/nbLigne >= zoneInterneMin && i/nbLigne < zoneInterneMax);
+
 }
 
 /**
@@ -32,34 +52,20 @@ void plaqueInit(int s) {
 	int nbCell = nbLigne*nbLigne;	
 	
 	printf("Will create %d row and line, so %d cell \n", nbLigne, nbCell);
-	cellule = (float*)malloc(nbCell*sizeof(float));
-	//printf("%p \n", cellule);
+	cellCurr = (float*)malloc(nbCell*sizeof(float));
 	
 	int i=0;
 	int j=0;
 	
-	// Calcul de la zone interne, dans laquelle la temperature est constante
-	int highBound = 1 << (s+3);
-	int lowBound = 1 << s;
-	
-	int zoneInterneMin = highBound - lowBound;
-	int zoneInterneMax = highBound + lowBound;
-	
 	for(i=0; i<nbCell; i++){
-		if(i%16 == 0)
-			j++;
 	
-		if(i%16 >= zoneInterneMin && i%16 < zoneInterneMax
-			&& j >= zoneInterneMin && j < zoneInterneMax)
-			cellule[i] = TEMP_CHAUD;
+		if(isZoneInterne(i,s))
+			cellCurr[i] = TEMP_CHAUD;
 		else
-			cellule[i] = TEMP_FROID;
+			cellCurr[i] = TEMP_FROID;
 	}
 	
-	//printf("2**n-1 : %d \n", 1 << (s+3));
-	//printf("2**n-4 : %d \n", 1 << (s));
-	
-	display(nbLigne);
+	display(cellCurr, nbLigne);
 }
 
 void printIntro(){
